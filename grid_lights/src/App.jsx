3d1654d9567778c167ae44b409e3ecf02e057208
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-01-24 00:10:21 +0530

    added grid lights

---
+++ grid_lights/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import './App.css'
+import Cell from './components/Cell'
+
+function App() {
+  const config = [
+    [1,1,1],
+    [1,0,1],
+    [1,1,1]
+  ]
+
+  const [order,setOrder] = useState([])
+  const [isDeactivating , setIsDeactivating] = useState(false)
+
+  const deactivateCells = () => {
+    setIsDeactivating(true)
+    const timer = setInterval(() => {
+      setOrder((origOrder)=>{
+          const newOrder = origOrder.slice()
+          newOrder.pop()
+
+          if(newOrder.length === 0){
+            clearInterval(timer)
+            setIsDeactivating(false)
+            
+          }
+          return newOrder
+      })
+    },300)
+  }
+
+  const activateCell = (index) => {
+      const newOrder = [...order ,index]
+      setOrder(newOrder)
+
+      //deactivate
+      if(newOrder.length === config.flat(1).filter(Boolean).length){
+        deactivateCells()
+      }
+  }
+
+  return (
+    <div className='wrapper'>
+      <div className='grid' style={{gridTemplateColumns:`repeat(${config[0].length},1fr)`}}>
+        {
+          config.flat(1).map((value,index)=> {
+            return value ? <Cell
+              key = {index}
+              label={`Cell ${index}`}
+              filled = {order.includes(index)}
+              onClick = {() => activateCell(index)}
+              isDisabled= {order.includes(index) || isDeactivating}
+            />:<span />
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ grid_lights/src/components/Cell.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cell = ({filled, onClick , isDisabled ,label}) => {
+  return (
+    <button type= 'button' onClick={onClick} className={filled ? "cell cell-activated" :"cell"} 
+    disabled={isDisabled}
+    aria-label={label}
+    />
+  )
+}
+
+export default Cell
